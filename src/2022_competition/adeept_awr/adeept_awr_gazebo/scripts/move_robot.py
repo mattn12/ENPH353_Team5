d9b9f390776bd345ef95e9c3e809c8e043274b36
mattn12 <matt.ngai12@gmail.com>
2022-11-16 15:22:35 -0800

    robot driving in circles

--- src/2022_competition/adeept_awr/adeept_awr_gazebo/scripts/move_robot.py
+++ src/2022_competition/adeept_awr/adeept_awr_gazebo/scripts/move_robot.py
@@ -25,53 +25,83 @@ class image_converter:
       print(e)
 
     (rows,cols,channels) = cv_image.shape
-    if cols > 60 and rows > 60 :
-      gray = cv2.cvtColor(cv_image, cv2.COLOR_RGB2GRAY)
-      color = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
 
-      #converting image frames to binary
-      threshold = 90
-      _, img_bin = cv2.threshold(gray, threshold, 255, cv2.THRESH_BINARY_INV)
+    # slice bottom portion of image
+    img_bot = cv_image[rows-100:,:]
+    gray = cv2.cvtColor(img_bot, cv2.COLOR_RGB2GRAY)
 
-      #draw contours
-      contours, hierarchy = cv2.findContours(img_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-      contour_color = (0, 255, 0)
-      contour_thickness = 5
-      img_contours = cv2.drawContours(img_bin, contours, -1, contour_color, contour_thickness)
+    #converting image frames to binary
+    threshold = 170
+    _, img_bin = cv2.threshold(gray, threshold, 255, cv2.THRESH_BINARY_INV)
 
-      #find middle of the line
-      M = cv2.moments(img_contours)
-      line_x = int(M['m10'] / M['m00'])
+    #draw contours
+    contours, hierarchy = cv2.findContours(img_bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contour_color = (0, 255, 0)
+    contour_thickness = 5
+    img_contours = cv2.drawContours(img_bin, contours, -1, contour_color, contour_thickness)
 
-      #draw circle
-      color = cv2.circle(color, (line_x, 800 - (2*20)), 20, (0,0,255), -1)
+    # check if m00 is zero (avoid division by 0)
+    # if the line is detected
+    if (len(contours)) > 0:
+        
+        #find middle of the line
+        M = cv2.moments(img_contours)
 
-    cv2.imshow("Image window", color)
+        # check if m00 is zero (avoid division by 0)
+        if (M['m00'] > 0):
+            self.timeout = 0
+
+            line_x = int(M['m10'] / M['m00'])
+
+            #draw circle
+            img_dot = cv2.circle(cv_image, (line_x, rows-100), 5, (0,0,255), -1)
+
+            # #determine which section the dot is in
+            # state_index = (int)(10*line_x/shape[1])
+            # if state_index >= 10:
+            #     state_index = 9
+            # state[state_index] = 1
+
+            # img_monitor = cv2.putText(img_dot, str(state),(0,15), cv2.FONT_HERSHEY_COMPLEX,0.4,(255,255,255),1)
+
+        # moment not found
+        else:
+            self.timeout += 1
+            if self.timeout > 10:
+                done = True
+
+    # if the line is not detected
+    else:
+        self.timeout += 1
+        if self.timeout > 10:
+            done = True
+
+    cv2.imshow("Image window", img_dot)
     cv2.waitKey(3)
 
 
 
     try:
-      rate = rospy.Rate(2)
+      rate = rospy.Rate(5)
       move = Twist()
-      move.linear.x = 1
+      move.linear.x = 0
       
-      # #Controller values
-      # tol = 40
-      # P_turn_scale = 0.015
-      # P_move_scale = 0.0005
-      # error = (400 - line_x)
+      #Controller values
+      tol = 40
+      P_turn_scale = 0.01
+      P_move_scale = 0.002
+      error = (400 - line_x)
 
-      # #P controllerer
-      # if(abs(error) > tol):
-      #   move.linear.x = 0.3 - abs(P_move_scale*error)
-      #   move.angular.z = P_turn_scale * error
-      # else:
-      #   move.angular.z = 0
+      #P controllerer
+      if(abs(error) > tol):
+        move.linear.x = 0.3 - abs(P_move_scale*error)
+        move.angular.z = P_turn_scale * error
+      else:
+        move.angular.z = 0
       
       #send move instructions to robot
       self.pub.publish(move)
-      rate.sleep(10)
+      rate.sleep()
     except CvBridgeError as e:
       print(e)
 
