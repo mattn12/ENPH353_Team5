f69c259bca018b500262950fa82a2b3d43a2d578
mattn12 <90164192+mattn12@users.noreply.github.com>
2022-11-21 22:26:36 -0800

    Delete build directory

--- build/2022_competition/adeept_awr/adeept_awr_gazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fizzer/ros_ws/src/2022_competition/adeept_awr/adeept_awr_gazebo
-# Build directory: /home/fizzer/ros_ws/build/2022_competition/adeept_awr/adeept_awr_gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/2022_competition/enph353/enph353_npcs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fizzer/ros_ws/src/2022_competition/enph353/enph353_npcs
-# Build directory: /home/fizzer/ros_ws/build/2022_competition/enph353/enph353_npcs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
