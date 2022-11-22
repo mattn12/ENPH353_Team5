a465ff9fc7fd376073df0a3adef528d5d0e9af93
mattn12 <matt.ngai12@gmail.com>
2022-11-21 22:45:29 -0800

    trying to find the plate

--- src/enph353_comp_controller/src/read_plate.py
+++ src/enph353_comp_controller/src/read_plate.py
@@ -35,15 +35,18 @@ class image_converter:
       print(e)
 
     # Constants
-    matching_tol = 0.9
+    matching_tol = 1
     img_height = 400
+    threshold = 90
     sharpen_kernel = np.array([[0,-1,0],[-1,5,-1],[0,-1,0]])
+    gaussianKernel = (5, 5)
     
     (rows,cols,channels) = cv_image.shape
 
     img_crop = cv_image[rows - img_height:,:]
     gray = cv2.cvtColor(img_crop, cv2.COLOR_BGR2GRAY)
-    gray = cv2.filter2D(src=img_crop,ddepth=-1,kernel=sharpen_kernel)
+    gray = cv2.GaussianBlur(gray, gaussianKernel, 0)
+    # gray = cv2.filter2D(src=img_crop,ddepth=-1,kernel=sharpen_kernel)
 
 
     #figure out wtf this does
@@ -53,8 +56,10 @@ class image_converter:
 
 
     # read template image
-    path = os.path.abspath(r"/home/matthew/ros_ws/src/2022_competition/enph353/enph353_gazebo/scripts/blank_plate.png")
-    img = cv2.imread(path)
+    path = os.path.join(os.path.dirname(__file__),'P_template (1).png')
+    img = cv2.imread(path,0)
+
+    # print(os.path.exists(path))
     
     # Find features of template image
     sift = cv2.xfeatures2d.SIFT_create()
@@ -79,21 +84,24 @@ class image_converter:
     # draw lines for matching keypoint
     matching = cv2.drawMatches(img,kp_img,gray,kp_gray,good_points,gray)
 
-    # # Homography
-    # query_pts = np.float32([kp_img[m.queryIdx].pt for m in good_points]).reshape(-1, 1, 2)
-    # train_pts = np.float32([kp_gray[m.trainIdx].pt for m in good_points]).reshape(-1, 1, 2)
-    # matrix, mask = cv2.findHomography(query_pts, train_pts, cv2.RANSAC, 5.0)
-    # #matches_mask = mask.ravel().tolist()
+    # Homography
+    query_pts = np.float32([kp_img[m.queryIdx].pt for m in good_points]).reshape(-1, 1, 2)
+    train_pts = np.float32([kp_gray[m.trainIdx].pt for m in good_points]).reshape(-1, 1, 2)
+    matrix, mask = cv2.findHomography(query_pts, train_pts, cv2.RANSAC, 5.0)
+    matches_mask = mask.ravel().tolist()
 
-    # # Perspective transform
-    # h, w = img.shape[0:2]
-    # pts = np.float32([[0, 0], [0, h], [w, h], [w, 0]]).reshape(-1, 1, 2)
-    # dst = cv2.perspectiveTransform(pts, matrix)
+    # Perspective transform
+    h, w = img.shape[0:2]
+    pts = np.float32([[0, 0], [0, h], [w, h], [w, 0]]).reshape(-1, 1, 2)
+    dst = cv2.perspectiveTransform(pts, matrix)
 
-    # #draw lines around object
-    # homography = cv2.polylines(cv_image, [np.int32(dst)], True, (0, 255, 0), 3)
+    #draw lines around object
+    homography = cv2.polylines(img_crop, [np.int32(dst)], True, (0, 255, 0), 3)
 
-    cv2.imshow("Circle Image",matching)
+    cv2.imshow("image1", homography)
+    cv2.waitKey(3)
+
+    cv2.imshow("image2", matching)
     cv2.waitKey(3)
 
     plate_num = 1
@@ -102,25 +110,25 @@ class image_converter:
 
 
 
-    # Code for starting the timer/sending plates
+    # # Code for starting the timer/sending plates
 
-    if self.startRun:
-        output = str('Team5,password,0,ABCD')
-        self.startRun = False
+    # if self.startRun:
+    #     output = str('Team5,password,0,ABCD')
+    #     self.startRun = False
 
-    # Test code
-    elif self.testPlate:
-        output = str('Team5,password,{},{}').format(plate_num,plate)
-        self.testPlate = False
+    # # Test code
+    # elif self.testPlate:
+    #     output = str('Team5,password,{},{}').format(plate_num,plate)
+    #     self.testPlate = False
 
-    else:
-        output = ''
+    # else:
+    #     output = ''
     
 
-    try:
-      self.license_pub.publish(output)
-    except CvBridgeError as e:
-      print(e)
+    # try:
+    #   self.license_pub.publish(output)
+    # except CvBridgeError as e:
+    #   print(e)
 
 def main(args):
   ic = image_converter()
